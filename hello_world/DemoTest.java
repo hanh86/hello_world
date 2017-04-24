a54e4970bd0b2f990a32cc0b1a60023a121b2664
hanh-dev <hanh.bm@teko.vn>
2017-04-24 16:27:51 +0700

    Thuc hanh cac lenh GIT

---
+++ hello_world/DemoTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+/**
+ * @author hanh_87
+ *
+ */
+
+package pkgDemo;
+public class DemoTest {
+	public int binhphuong(int a){
+		return a*a;
+	}
+	
+	public String concat(boolean append, String a, String b){
+		String result = null;
+		if (append){
+			result = a+b;
+			
+		}
+		return result.toLowerCase();
+	}
+	
+
+}
